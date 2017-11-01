89988bd77e281ecd66118b8ff285c731fa44c0a0
Maeva L <maeva.lauzier@gmail.com>
2017-11-01 23:24:34 +0100

    Grid and camera Raycast to track mouse input to define start and end of the future algorithm

---
+++ AStar/Assets/Scripts/ClickEvent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickEvent : MonoBehaviour {
+    public Camera camera;
+    private int cpt;
+    private Ray ray;
+    private RaycastHit hit;
+    private Transform start;
+    private Transform end;
+
+    // Use this for initialization
+    void Start() {
+        cpt = 0; 
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (cpt < 2) {
+            if (Input.GetMouseButtonDown(0)) {
+            cpt += 1;
+            
+                ray = camera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit)) {
+                    Transform objectHit = hit.transform;
+                    if(cpt == 1) {
+                        start = objectHit;
+                    } else {
+                        end = objectHit;
+                    }
+                    objectHit.gameObject.GetComponent<Renderer>().material.color = Color.green;
+                }
+            }
+        }
+           
+        
+    }
+}
+
---
+++ AStar/Assets/Scripts/Grid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+    public GameObject cell;
+    public Vector3 gridSize;
+
+	// Use this for initialization
+	void Start () {
+        GenerateGrid();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void GenerateGrid() {
+        for(int i = 0; i < gridSize.x; i++) {
+            for(int j = 0; j < gridSize.z; j++) {
+                
+                Instantiate(cell, new Vector3(i, 0, j), Quaternion.identity);
+            }
+        }
+    }
+}
